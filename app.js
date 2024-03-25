58f8b3121a7a6568df8763658e21c0f27cfcd315
josearcos2023 <jose.arcos@tecsup.edu.pe>
2024-03-24 23:55:11 -0500

    Ejercicio 3

--- app.js
+++ app.js
@@ -2,32 +2,13 @@ const express = require('express');
 const port = 3500;
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('Hello world')
-})
+app.arguments(express.text())
+app.arguments(express.json())
+app.arguments(express.urlencoded({extended: false}))
 
-app.get('/myfile', (req,res)=>{
-    res.sendFile('./arquitectura.png'),{
-        root: __dirname
-    }
-})
-
-app.get('/user',(req,res)=>{
-    res.json({ 
-        nombre:"José",
-        apellido:"Arcos",
-        edad:42,
-        points: [1, 2, 3],
-        address: {
-            ciudad: "Lima - Peru",
-            distrito: "San Martin de Porres",
-            calle: "Jr. Ica"
-        }       
-    })
-})
-
-app.get('/isAlive',(req,res)=>{
-    res.sendStatus(204)
+app.post('/user',(req,res)=>{
+    console.log(req.body)
+    res.send('Nuevo usuario creado')
 })
 
 app.listen(port, () => {
