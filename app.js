c847b7353036566e45c141e0f62bb9e6a2eafeb0
josearcos2023 <jose.arcos@tecsup.edu.pe>
2024-03-25 00:07:21 -0500

    Ejercicio 4 - cambio de simbolo para interpolación

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const port = 3500;
-const app = express();
+const express = require('express')
+const port = 3500
+const app = express()
 
-app.use(express.text())
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.length('/hello/:username', (req, res)=>{
+    //console.log(req.params)
+    console.log(typeof req.params.username)
+    res.send(`Hello ${req.params.username.toUpperCase()}`)
+})
 
-app.post('/user',(req,res)=>{
-    console.log(req.body)
-    res.send('Nuevo usuario creado')
+app.get('/add/:x/:y',(req,res)=>{
+    const {x, y} = req.params
+    res.send(`Result: ${ parseInt(x) + parseInt(y)}`)
+})
+
+app.get('/user/:username/photo', (req,res) => {
+    if(req.params.username.toUpperCase() === "JOSE"){
+        return res.sendFile('./arquitectura.png', {
+            root: __dirname
+        })
+    }
+    res.send('El usuario no tiene acceso......!!!')
+})
+
+app.get('/name/:name/age/:age', (req,res) => {
+    res.send(`El usuario ${req.params.name} tiene ${req.params.age} años...!!!`)
 })
 
 app.listen(port, () => {
-    console.log('Server on port ${port}')
+    console.log(`Server on port ${port}`)
 });
